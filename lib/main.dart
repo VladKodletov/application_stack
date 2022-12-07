de22b791491d2860c936cafc0d9ee6504b1fe4cd
Vlad <funnytrofi@gmail.com>
2022-12-07 14:04:24 +0600

    folder to clean arch

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:application_websocket/features/mainscreen/presentation/mainscreen.dart';
+import 'package:application_websocket/features/mainscreen/presentation/screen/mainscreen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
