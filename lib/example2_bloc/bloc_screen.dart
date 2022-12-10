de73177e59dca6e35002a4def5f4860a0c0c8e7b
Vlad <funnytrofi@gmail.com>
2022-12-10 23:35:40 +0600

    add example2

---
+++ lib/example2_bloc/bloc_screen.dart
@@ -0,0 +1,32 @@
+// import 'package:application_websocket/example_bloc/bloc.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter_bloc/flutter_bloc.dart';
+
+// class BlocScreen2 extends StatelessWidget {
+//   const BlocScreen2({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Center(),
+//     );
+//   }
+// }
+
+// Stream<int> exampleStream(int max) async* {
+//   for (int i = 0; i < max; i++) {
+//     yield i;
+//   }
+// }
+
+// Future<int> sumStream(Stream<int> stream) async {
+//   int sum = 0;
+//   await for (int value in stream) {
+//     sum += value;
+//   }
+//   return sum;
+// }
+
+void main(List<String> args) {
+  print(3);
+}
